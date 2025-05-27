c62bb9f7bb3a040361bb796590317199f9f01bb2
Priti Mahale <pritimahale152001@gmail.com>
2025-05-27 17:12:56 +0530

    msg

---
+++ Backend/basics_project/src/looping_statement/DoWhileProgram.java
@@ -0,0 +1,23 @@
+package looping_statement;
+
+public class DoWhileProgram {
+
+	public static void main(String[] args) {
+		 int a=1;
+		 do {
+			 a++;
+			 System.out.println("hello world");
+		 }while(a<=10);
+	}
+}
+
+
+/*
+ * 1.Idly
+ * 2.Dosa
+ * 3.Vada
+ * 4.Upma
+ * 5.Pongal
+ * 
+ * 
+ * */
---
+++ Backend/basics_project/src/looping_statement/WhileProgram1.java
@@ -0,0 +1,11 @@
+package looping_statement;
+
+public class WhileProgram1 {
+	public static void main(String[] args) {
+        int a=1;
+		while (a<=10) {
+			System.out.println("hello world");
+            a++;
+		}
+	}
+}
